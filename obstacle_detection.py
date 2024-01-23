7920a49841add8374cd45e97ac15cc3c6ef0c617
Laiba <laibarao86@gmail.com>
2024-01-23 22:37:37 +0500

    Adding obstacle detection

---
+++ obstacle_detection.py
@@ -0,0 +1,11 @@
+class ObstacleDetection:
+    def __init__(self):
+        pass
+
+    def detect_obstacles(self):
+        # Code to detect obstacles in the path
+        print("Obstacles detected in the vicinity.")
+
+if __name__ == "__main__":
+    obstacle_detection = ObstacleDetection()
+    obstacle_detection.detect_obstacles()
