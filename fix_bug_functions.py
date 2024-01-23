dbb0118460beaa758a386fde9d558d4774afc41f
Laiba <laibarao86@gmail.com>
2024-01-23 22:42:41 +0500

    Fixing bugs

---
+++ fix_bug_functions.py
@@ -0,0 +1,14 @@
+def fix_bug_1():
+    # Add code to fix bug 1
+    print("Bug 1 has been fixed!")
+
+def fix_bug_2():
+    # Add code to fix bug 2
+    print("Bug 2 has been fixed!")
+
+# You can add more functions to fix additional bugs
+
+if __name__ == "__main__":
+    # You can add test cases or an example of how to use the bug-fix functions
+    fix_bug_1()
+    fix_bug_2()
