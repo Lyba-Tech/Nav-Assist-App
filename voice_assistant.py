d3274daa8ec55245414ac6c336acdfb1821a7ceb
Laiba <laibarao86@gmail.com>
2024-01-23 23:01:01 +0500

    Adding voice feature

--- voice_assistant.py
+++ voice_assistant.py
@@ -9,3 +9,6 @@ class VoiceAssistant:
 if __name__ == "__main__":
     voice_assistant = VoiceAssistant()
     voice_assistant.provide_navigation_instructions("Turn left at the next intersection.")
+
+    #some changes
+    print("This code contain information about obstacles")
